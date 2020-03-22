55d28fd3a3fe411cee591222469e96d25d9d1cc6
章文炳 <zhangwenbing@u51.com>
2020-03-22 18:19:59 +0800

    基本的逻辑完成

--- index/views.py
+++ index/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
-from django.http.response import JsonResponse, HttpResponse
+from django.shortcuts import render, reverse, redirect
+from django.http.response import JsonResponse, HttpResponse, HttpResponseRedirect
 from .models import Message
+from .forms import MessageForm
 from django.template import loader
+from django.contrib import messages as django_messages
 
 
 # Create your views here.
 def index(request):
+    message_form = MessageForm()
+    if request.method == 'POST':
+        message_form = MessageForm(request.POST)
+        if message_form.is_valid():
+            message_form.save()
+            django_messages.success(request, '留言成功')
+            return redirect(reverse('index'))
+
     messages = Message.objects.all()
     context = {
-        'messages': messages,
+        'message_list': messages,
+        'form': message_form
     }
     return render(request, 'index/index.html', context)
-
